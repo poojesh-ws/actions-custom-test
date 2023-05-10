fa955b4ee2ef34ae4ed6b96995ff9431eef45293
Poojesh Shetty <poojesh@wednesday.is>
2023-05-10 14:59:46 +0530

    feat: initial setup

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+json_string = ${INPUT_JSON_VALUE} 
+echo $json_string
+
+$(${json_string} >> "credential.json")
+
+#check file created
+$(ls -la)
+
+#set github env 
+# echo "cred=$($)" >> $GITHUB_ENV
+
+echo "added value to env"
